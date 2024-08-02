9143d9aaf5b69da763ce4ad95ae7500a4764a0e8
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-08-02 15:54:56 +0530

    create post data model

---
+++ backend/src/Models/post.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        require: true
+    }, 
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    category: {
+        type: String,
+        default: "Uncategorised"
+    },
+    imagesURLs: [{
+        url: { 
+            type: String 
+        },
+        caption: { 
+            type: String 
+        }
+      }],
+      videoURLs: [{
+        url: {
+            type: String,
+        }, 
+        caption: {
+            type: String,
+        }
+      }]
+
+},{timestamps: true})
+
+export Post = new mongoose.model("Post", postSchema);
---
+++ backend/src/Routes/posts.routes.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const router = express.Router();
+
+
+router.route("/create-post")
+
+
+
+
+
+
+export default router;
