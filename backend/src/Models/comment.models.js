5b6f8a5d0d2d0e20f00e746d9fa9089b9196495f
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-08-05 14:17:49 +0530

    add comment schema

---
+++ backend/src/Models/comment.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const commentSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    parent: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment',
+        default: null,
+    },
+    replies:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }] 
+
+}, {timestamps: true})
+
+const Comment = new mongoose.model("Comment", commentSchema);
+export { Comment }
