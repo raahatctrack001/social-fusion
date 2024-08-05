1fccbad282066c6c3d47303e258f841cd0d54469
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-08-05 15:25:12 +0530

    add frontend field file structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom"
+
+function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" />
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App
